d4ebb812f6cdcefafc83d529dce93a8f0828bb27
Charles Bedrosian <charles@bedrosian.com>
2012-06-08 00:03:04 -0500

    install ruby via RVM and update gemsets with RVM

--- lib/ruby.sh
+++ lib/ruby.sh
@@ -7,7 +7,7 @@ function install_rvm
 {
 	#borrowed from http://www.linode.com/stackscripts/view/?StackScriptID=2438
 	log "Installing RVM and Ruby dependencies"
-	apt-get -y install curl git-core bzip2 build-essential zlib1g-dev libssl-dev
+	apt-get -y install libgemplugin-ruby build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
 
 	log "Installing RVM system-wide"
 	curl -L get.rvm.io | sudo bash -s stable
@@ -31,20 +31,19 @@ function install_ruby
 	#borrowed from http://www.linode.com/stackscripts/view/?StackScriptID=2438
 	log "Installing Ruby 1.9.3-$RUBY_RELEASE"
 
-	rvm install 1.9.3-$RUBY_RELEASE
+	rvm --install 1.9.3-$RUBY_RELEASE
 	rvm use 1.9.3-$RUBY_RELEASE --default
-
-	log "setup gems package and other dependencies"
-	apt-get -y install libgemplugin-ruby build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
 
 	log "ruby version information"
 	ruby -v
 	gem -v
-	rvm -v
+    rvm -v
+
+	create_gemrc
 
 	log "Updating Ruby gems"
-	set_production_gemrc
-	gem update --system
+	rvm get stable
+	rvm rubygems latest
 }
 
 function create_gemrc {
